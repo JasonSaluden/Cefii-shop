5a92fb783d4915da3929891dac31c6c0550bf7bd
Maxwell <axel.paillard.12@gmail.com>
2026-04-01 16:46:55 +0200

    add

--- cefiishop/front/src/App.test.jsx
+++ cefiishop/front/src/App.test.jsx
@@ -3,8 +3,8 @@ import { render } from '@testing-library/react';
 import App from './App';
 
 describe('App Component', () => {
-  it('should render without crashing', () => {
-    const { container } = render(<App />);
-    expect(container).toBeTruthy();
-  });
+    it('should render without crashing', () => {
+        const { container } = render(<App />);
+        expect(container).toBeTruthy();
+    });
 });
